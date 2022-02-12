c76b7fa14331cdfd297ed7694865ea23f5fe838f
jianminggit <99082662+jianminggit@users.noreply.github.com>
2022-02-12 17:17:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ setInterval(intervalFunc, 100000);
 
 // Self call to hosted address to prevent heroku from sleeping
 var server = http.createServer(function (req, res) {
-    console.log('t')
+    console.log('self call')
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
